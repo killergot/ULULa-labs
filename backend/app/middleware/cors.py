cbc0c3d54e4e0af3b17fbc7d66556bc3cdb80356
killergot <disney.132435morozko@mail.ru>
2025-06-10 03:00:41 -0300

    update CORS

--- backend/app/middleware/cors.py
+++ backend/app/middleware/cors.py
@@ -4,7 +4,8 @@ def get_cors_middleware(app):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            '*'
+            'http://185.95.159.198/',
+            'https://185.95.159.198/'
         ],
         allow_credentials=True,
         allow_methods=["GET", "POST", "PUT", "DELETE","PATCH"],  # Ограничить по необходимости
